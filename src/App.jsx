90cf9b4ccebcb95610a4a174b48eed0d4db71d73
Jofi <jofi1@seznam.cz>
2024-03-07 22:58:48 +0100

    posun zavorky

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import rawData from "./dogsData.json"
-
+import rawData from "./dogsData.json";
 
 function App() {
   const [listOfDogs, setListOfDogs] = useState(rawData.dogs);
 
-  useEffect(())=>{console.log(listOfDogs)},[listOfDogs];
+  useEffect(() => {
+    console.log(listOfDogs);
+  }, [listOfDogs]);
 
   return <div className="App">čisto</div>;
 }
