b0ae5db046e78fe2f292efb16b52fd23d7d640d9
Jofi <jofi1@seznam.cz>
2024-03-07 22:53:00 +0100

    import dat, useState, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+import rawData from "./dogsData.json"
+
+
 function App() {
+  const [listOfDogs, setListOfDogs] = useState(rawData.dogs);
+
+  useEffect(())=>{console.log(listOfDogs)},[listOfDogs];
+
   return <div className="App">čisto</div>;
 }
 
